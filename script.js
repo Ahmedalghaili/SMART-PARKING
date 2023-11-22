878a0d16da41da960a1525e169fe75df25bba2cb
Ahmedalghaili <136468427+Ahmedalghaili@users.noreply.github.com>
2023-11-22 11:47:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    // Your map initialization code here
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 37.7749, lng: -122.4194 },
+        zoom: 13,
+    });
+
+    // Additional map functionality can be added here based on your requirements
+}
